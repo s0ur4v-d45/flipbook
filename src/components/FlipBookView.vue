40e9fd75e19251972d92399b3a3e69baa3ee2198
Sourav Das <73391171+s0ur4v-d45@users.noreply.github.com>
2025-02-20 01:17:14 +0530

    added pdf (#1)
    Co-authored-by: abhi <abhi>

--- src/components/FlipBookView.vue
+++ src/components/FlipBookView.vue
@@ -1,105 +1,106 @@
 <template>
-  <div class="flipbook-container">
-  
-    <div class="flipbook">
-      <div class="page cover front" @click="flipPage">Cover Page</div>
-      <div class="page spread" @click="flipPage">
-        <div class="left">Page 5</div>
-        <div class="right">Page 6</div>
+  <div>
+    <input type="file" @change="handleFileUpload" />
+    <div id="flipbook">
+      <div v-for="(page, index) in pages" :key="index">
+        <img :src="page" alt="Page" />
       </div>
-      <div class="page spread" @click="flipPage">
-        <div class="left">Page 3</div>
-        <div class="right">Page 4</div>
-      </div>
-      <div class="page spread" @click="flipPage">
-        <div class="left">Page 1</div>
-        <div class="right">Page 2</div>
-      </div>
-      
     </div>
+    <button @click="prevPage">Previous</button>
+    <button @click="nextPage">Next</button>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
+import "turn.js";
+import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
+
 export default {
   name: "FlipBook",
   data() {
     return {
-      currentPage: 0
+      pages: [],
     };
   },
   methods: {
-    flipPage(event) {
-      const page = event.currentTarget;
-      if (!page.classList.contains("flipped")) {
-        page.classList.add("flipped");
-      } else {
-        page.classList.remove("flipped");
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = (e) => {
+          this.loadPDF(e.target.result);
+        };
       }
-    }
-  }
+    },
+
+    async loadPDF(pdfUrl) {
+      try {
+        GlobalWorkerOptions.workerSrc =
+          "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js";
+
+        const pdf = await getDocument(pdfUrl).promise;
+        const totalPages = pdf.numPages;
+
+        this.pages = [];
+
+        for (let i = 1; i <= totalPages; i++) {
+          const page = await pdf.getPage(i);
+          const canvas = document.createElement("canvas");
+          const context = canvas.getContext("2d");
+          const viewport = page.getViewport({ scale: 2 });
+
+          canvas.width = viewport.width;
+          canvas.height = viewport.height;
+
+          await page.render({ canvasContext: context, viewport }).promise;
+          this.pages.push(canvas.toDataURL("image/png"));
+        }
+
+        // Wait until pages are loaded, then initialize Flipbook
+        this.$nextTick(() => {
+          this.initFlipbook();
+        });
+      } catch (err) {
+        console.error("Error loading PDF:", err);
+      }
+    },
+
+    initFlipbook() {
+      this.$nextTick(() => {
+        if (window.jQuery) {
+          $("#flipbook").turn({
+            width: 800,
+            height: 500,
+            autoCenter: true,
+            display: "double",
+          });
+        } else {
+          console.error("jQuery is not loaded properly.");
+        }
+      });
+    },
+
+    nextPage() {
+      $("#flipbook").turn("next");
+    },
+
+    prevPage() {
+      $("#flipbook").turn("previous");
+    },
+  },
 };
 </script>
 
 <style>
-.flipbook-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f0f0f0;
-  position: relative;
+#flipbook {
+  width: 800px;
+  height: 500px;
+  margin: auto;
 }
-.flipbook {
-  display: flex;
-  flex-direction: column;
-  perspective: 1500px;
-  position: relative;
-  width: 320px;
-  height: 450px;
-}
-.page {
+img {
   width: 100%;
   height: 100%;
-  margin: 0;
-  background: white;
-  border: 1px solid #aaa;
-  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);
-  transition: transform 1s ease-in-out;
-  transform-origin: left center;
-  position: absolute;
-  backface-visibility: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2em;
-  font-weight: bold;
-}
-.cover {
-  background: linear-gradient(45deg, #ff7e5f, #feb47b);
-  color: white;
-  text-align: center;
-  z-index: 5;
-}
-.spread {
-  display: flex;
-  flex-direction: row;
-  z-index: 2;
-}
-.spread .left, .spread .right {
-  flex: 1;
-  text-align: center;
-  line-height: 450px;
-  border-right: 1px solid #ccc;
-  background: white;
-  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.1);
-}
-.spread .right {
-  border-right: none;
-}
-.flipped {
-  transform: rotateY(-180deg);
-  z-index: 1;
 }
 </style>
-
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /pdf\.worker\.(min\.)?js$/,
+          use: "worker-loader"
+        }
+      ]
+    }
+  }
+};
