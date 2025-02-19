7d003f376c7a4c7e4b5e8f9c8b1cc555baed139b
Sourav Das <73391171+s0ur4v-d45@users.noreply.github.com>
2025-02-20 01:19:01 +0530

    Revert "added pdf (#1)" (#2)
    This reverts commit 40e9fd75e19251972d92399b3a3e69baa3ee2198.

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /pdf\.worker\.(min\.)?js$/,
-          use: "worker-loader"
-        }
-      ]
-    }
-  }
-};
